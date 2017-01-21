9e8d208d7a8ca325793da4d757a82b5f052298d1
FedePic <fedepic@gmail.com>
2017-01-21 22:14:04 +0100

    added Timer script

---
+++ ProgettoFantogrammi/Assets/Script/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float counterTime;
+    public bool isPaused;
+
+	void Start () {
+        counterTime = 0f;
+        isPaused = true;
+	}
+
+    public void Pause()
+    {
+        isPaused = !isPaused;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log(counterTime);
+        if (isPaused == false)
+        {
+            counterTime = counterTime + Time.deltaTime;
+        }
+	}
+}
