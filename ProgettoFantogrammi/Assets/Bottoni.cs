6e1fe2e69dbee1791c4fe4a9bb9618b37684b352
Manuel Salvadori <morke.bm@hotmail.it>
2017-01-22 08:35:37 +0100

    sono le 8 e mezza e ho sonno

--- ProgettoFantogrammi/Assets/Bottoni.cs
+++ ProgettoFantogrammi/Assets/Bottoni.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 
-public class Bottoni : MonoBehaviour {
+public class Bottoni : MonoBehaviour
+{
 
 	public void reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void next() {
-        SceneManager.LoadScene("Scena" + (Int32.Parse(SceneManager.GetActiveScene().name.Substring(5)) + 1));
+    public void next()
+    {
+        int sceneToLoad = (Int32.Parse(SceneManager.GetActiveScene().name.Substring(5)) + 1);
+        if (sceneToLoad < 4)
+        {
+            SceneManager.LoadScene("Scena" + sceneToLoad);
+        }
+        else
+        {
+            back();
+        }
     }
 
     public void back()
