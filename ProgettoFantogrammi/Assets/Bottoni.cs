5ba3066b1122ad3f12046d9e9ffe40fe664c56e6
Matteo Marras <matteo.marras1994@gmail.com>
2017-01-22 07:30:44 +0100

    ohohobobiodarthjjb

--- ProgettoFantogrammi/Assets/Bottoni.cs
+++ ProgettoFantogrammi/Assets/Bottoni.cs
@@ -14,4 +14,18 @@ public class Bottoni : MonoBehaviour {
     public void next() {
         SceneManager.LoadScene("Scena" + (Int32.Parse(SceneManager.GetActiveScene().name.Substring(5)) + 1));
     }
+
+    public void back()
+    {
+        Scene sc = SceneManager.GetActiveScene();
+        Camera.main.GetComponent<AudioListener>().enabled = false;
+        SceneManager.LoadScene("Menu");
+        StartCoroutine(aspetta());
+    }
+
+    IEnumerator aspetta()
+    {
+        yield return new WaitForSeconds(1f);
+        GameObject.FindGameObjectWithTag("MainMenu").GetComponent<MenuManager>().startButton();
+    }
 }
---
+++ ProgettoFantogrammi/Assets/Starter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour {
+
+    public bool start = false;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
