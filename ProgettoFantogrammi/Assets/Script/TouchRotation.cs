44999bb2f5fd9934b3895d8d7043c7a8cfee44f7
Matteo Marras <matteo.marras1994@gmail.com>
2017-01-22 01:32:46 +0100

    Vendettona3

--- ProgettoFantogrammi/Assets/Script/TouchRotation.cs
+++ ProgettoFantogrammi/Assets/Script/TouchRotation.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TouchRotation : MonoBehaviour
 {
 
 	public float rotationRate = 1.5f;
+    public Text testo;
 
-	void Update ()
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(360*Random.Range(0f,1f), 360 * Random.Range(0f, 1f), 0f));
+    }
+
+    void Update ()
 	{
 		foreach (Touch touch in Input.touches)
 		{
-			if (touch.phase == TouchPhase.Moved)
+            Debug.Log(Screen.currentResolution.width);
+            Debug.Log(Screen.currentResolution.height);
+
+            if (touch.phase == TouchPhase.Moved)
 			{
-                transform.Rotate((touch.deltaPosition.y) * rotationRate * Time.deltaTime, 0f,  -(touch.deltaPosition.x * rotationRate * Time.deltaTime), Space.World);
-			}
-		}
+                transform.Rotate((touch.deltaPosition.y) / Screen.currentResolution.height * rotationRate * Time.smoothDeltaTime, 0f,  -(touch.deltaPosition.x / Screen.currentResolution.width * rotationRate * Time.smoothDeltaTime), Space.World);
+            }
+            testo.text = ((touch.deltaPosition.y) / Screen.currentResolution.height * rotationRate * Time.smoothDeltaTime).ToString();
+        }
+
+
 	}
+
 }
